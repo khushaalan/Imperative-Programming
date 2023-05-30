9604cc4821b33e394847ee97511723bd54c2dfb1
Khushaalan Arjunan <khushaalanarjunan@gmail.com>
2023-05-31 00:37:00 +0800

    'update'

--- LAB01-TASK03.c
+++ LAB01-TASK03.c
@@ -11,9 +11,25 @@ Group Members:  1. Khushaalan Arjunan (A190409)
 
 #include <stdio.h>
 #include<stdbool.h>
+#define ll long long int
 
 int main(){
+    printf("Enter a positive integer: ");
+    ll num, factorial=1;
+    bool isNegative = false;
+    scanf("%lld", &num);
 
+    isNegative = (num<0 ? true:false);
+    for(ll i = 1; i <= num; i++){
+        factorial *= i;
+    }
+
+
+    if(isNegative){
+        printf("Please enter a positive integer.Please try again.\n");
+    }else{
+        printf("Number: %lld\nFactorial: %lld\n", num, factorial);
+    }
 
     return 0;
 }
--- script.py
+++ script.py
@@ -2,36 +2,34 @@
 import os
 
 def compileTask1():
-    os.system("clang -w LAB01-TASK01.c -o LAB01-TASK01.exe && LAB01-TASK01.exe")
+    os.system("gcc -w LAB01-TASK01.c -o LAB01-TASK01.exe && LAB01-TASK01.exe")
     if os.path.exists("LAB01-TASK01.exe"):
         os.remove("LAB01-TASK01.exe")
 
 def compileTask2():
-    os.system("clang -w LAB01-TASK02.c -o LAB01-TASK02.exe && LAB01-TASK02.exe")
+    os.system("gcc -w LAB01-TASK02.c -o LAB01-TASK02.exe && LAB01-TASK02.exe")
     if os.path.exists("LAB01-TASK02.exe"):
         os.remove("LAB01-TASK02.exe")
 
 def compileTask3():
-    os.system("clang -w LAB01-TASK03.c -o LAB01-TASK03.exe && LAB01-TASK03.exe")
+    os.system("gcc -w LAB01-TASK03.c -o LAB01-TASK03.exe && LAB01-TASK03.exe")
     if os.path.exists("LAB01-TASK03.exe"):
         os.remove("LAB01-TASK03.exe")
 
 def compileTask4():
-    os.system("clang -w LAB01-TASK04.c -o LAB01-TASK04.exe && LAB01-TASK04.exe")
+    os.system("gcc -w LAB01-TASK04.c -o LAB01-TASK04.exe && LAB01-TASK04.exe")
     if os.path.exists("LAB01-TASK04.exe"):
         os.remove("LAB01-TASK04.exe")
 
 def compileTask5():
-    os.system("clang -w LAB01-TASK05.c -o LAB01-TASK05.exe && LAB01-TASK05.exe")
+    os.system("gcc -w LAB01-TASK05.c -o LAB01-TASK05.exe && LAB01-TASK05.exe")
     if os.path.exists("LAB01-TASK05.exe"):
         os.remove("LAB01-TASK05.exe")
 
 
 
-
-
 # compileTask1()
 # compileTask2()
-# compileTask3()
+compileTask3()
 # compileTask4()
 # compileTask5()
