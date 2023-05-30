acd9fd8eaa4ea5e474fbe30d5dda83ca425bed8e
Khushaalan Arjunan <khushaalanarjunan@gmail.com>
2023-05-30 23:18:13 +0800

    'update'

--- script.py
+++ script.py
@@ -2,34 +2,36 @@
 import os
 
 def compileTask1():
-    os.system("gcc LAB01-TASK01.c -o LAB01-TASK01 && LAB01-TASK01")
+    os.system("clang -w LAB01-TASK01.c -o LAB01-TASK01.exe && LAB01-TASK01.exe")
     if os.path.exists("LAB01-TASK01.exe"):
         os.remove("LAB01-TASK01.exe")
 
 def compileTask2():
-    os.system("gcc LAB01-TASK02.c -o LAB01-TASK02 && LAB01-TASK02")
+    os.system("clang -w LAB01-TASK02.c -o LAB01-TASK02.exe && LAB01-TASK02.exe")
     if os.path.exists("LAB01-TASK02.exe"):
         os.remove("LAB01-TASK02.exe")
 
 def compileTask3():
-    os.system("gcc LAB01-TASK03.c -o LAB01-TASK03 && LAB01-TASK03")
+    os.system("clang -w LAB01-TASK03.c -o LAB01-TASK03.exe && LAB01-TASK03.exe")
     if os.path.exists("LAB01-TASK03.exe"):
         os.remove("LAB01-TASK03.exe")
 
 def compileTask4():
-    os.system("gcc LAB01-TASK04.c -o LAB01-TASK04 && LAB01-TASK04")
+    os.system("clang -w LAB01-TASK04.c -o LAB01-TASK04.exe && LAB01-TASK04.exe")
     if os.path.exists("LAB01-TASK04.exe"):
         os.remove("LAB01-TASK04.exe")
 
 def compileTask5():
-    os.system("gcc LAB01-TASK05.c -o LAB01-TASK05 && LAB01-TASK05")
+    os.system("clang -w LAB01-TASK05.c -o LAB01-TASK05.exe && LAB01-TASK05.exe")
     if os.path.exists("LAB01-TASK05.exe"):
         os.remove("LAB01-TASK05.exe")
 
 
 
+
+
 # compileTask1()
-compileTask2()
+# compileTask2()
 # compileTask3()
 # compileTask4()
 # compileTask5()
