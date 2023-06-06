29517170d9be56374a5987990d83e208f63dcc97
khushaalan <khushaalanarjunan@gmail.com>
2023-06-07 00:03:46 +0800

    'update'

--- LAB01-TASK01.c
+++ LAB01-TASK01.c
@@ -7,65 +7,78 @@
                     * 3. Stuart Heng Fu Yu (A189660)
                     * 4. Zhao Zhao (A184841)
  *
+ * This program prompts the user to enter their score between 0 and 100.
+ * It then calculates and displays the corresponding grade based on the score.
+ * The grade calculation uses an if...else construct to identify the range the score falls into.
+ * User input validation and error handling are implemented to ensure input is within the valid range.
+ *
  **********************************************************************/
 
+/* Necessary libraries are included. */
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <inttypes.h>
 
-
+/* A structure, ScoreInput, is defined to store the user input as a string and as a float, and to record whether the input is valid. */
 struct ScoreInput {
-    bool is_valid; 
+    bool is_valid;
     float score;
     char str[10];
 };
 
-/**
- * get_valid_score() - Get valid score input from the user.
- *
- * This function prompts the user to enter a score and validates the input.
- * 
- * It reads the input as a string using scanf and then attempts to convert it
- * to a float value using sscanf. 
- * If the conversion is successful, the input is considered valid and stored in the ScoreInput structure. 
- * If the conversion fails, the input is considered invalid and is_valid field is set to false.
- *
- * Returns:
- *     ScoreInput structure containing the validated score input.
- */
+/* The is_float function checks if a given string can be converted to a float by ensuring it contains only digits and at most one decimal point. */
+bool is_float(const char* str) {
+    bool point_seen = false;
+    for (int64_t i = 0; i < strlen(str); i++) {
+        if (str[i] == '.') {
+            if (point_seen) {
+                return false;
+            }
+            point_seen = true;
+        } else if (!isdigit(str[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/* The get_valid_score function reads the user input, validates it, and returns a ScoreInput structure with the results. */
 struct ScoreInput get_valid_score() {
     struct ScoreInput user_score;
     user_score.is_valid = true;
 
-    /**
-     * %5[^\t\n ] - Matches up to 5 characters that are not a tab, newline, or space.
-     * This is to prevent buffer overflow.
-    */
-    scanf("%5[^\t\n ]", user_score.str);
+    /* First, the function attempts to read the user input as a string up to a newline character. If this fails, the function sets is_valid to false and returns. */
+    if (scanf("%9[^\n]", user_score.str) != 1) {
+        user_score.is_valid = false;
+        return user_score;
+    }
+
+    /* Next, the function attempts to convert the string to a float. If this fails, the function sets is_valid to false. */
     user_score.is_valid = (sscanf(user_score.str, "%f", &user_score.score) == 1 ? true : false);
+
+    /* If the conversion to float was successful, the function checks whether the string is a valid float using the is_float function. */
+    if (user_score.is_valid) {
+        user_score.is_valid = is_float(user_score.str);
+    }
 
     return user_score;
 }
 
-/**
- * main() - The entry point of the program.
- *
- * This function calls the get_valid_score() function to get a valid score input from the user.
- * Based on the input, it determines if the score is valid and falls within the range of 0 to 100. 
- * If the score is valid, it determines the grade based on the score and prints the score and grade.
- * If the score is invalid or outside the valid range, it displays an error message.
- *
- * Returns:
- *     0 on successful execution.
- */
+/* The main function of the program. */
 int main() {
+    /* The user is prompted to enter their score. */
     printf("Enter your score: ");
     struct ScoreInput score = get_valid_score();
 
+    /* If the input was not valid, an error message is printed, and the program terminates. */
     if (score.is_valid == false) {
         printf("Invalid score. Please enter a score between 0 and 100.\n");
         return 0;
     }
 
+    /* If the input is valid, the program checks which range the score falls into and prints the score and the corresponding grade. */
     if (score.score >= 0 && score.score <= 100) {
         if (score.score >= 90 && score.score <= 100) {
             printf("Score: %.1f\nGrade: A+\n", score.score);
