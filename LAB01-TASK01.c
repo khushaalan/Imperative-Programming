50ebc4384e4ed6aac46bc62f2e9eabdfcb57f61e
khushaalan <khushaalanarjunan@gmail.com>
2023-06-07 00:59:21 +0800

    'update'

--- LAB01-TASK01.c
+++ LAB01-TASK01.c
@@ -2,10 +2,10 @@
  * LAB01-TASK01: Grade Calculator
  *
  * Group Members:
-                    * 1. Khushaalan Arjunan (A190409)
-                    * 2. Wang Rui (A184975)
-                    * 3. Stuart Heng Fu Yu (A189660)
-                    * 4. Zhao Zhao (A184841)
+ * 1. Khushaalan Arjunan (A190409)
+ * 2. Wang Rui (A184975)
+ * 3. Stuart Heng Fu Yu (A189660)
+ * 4. Zhao Zhao (A184841)
  *
  * This program prompts the user to enter their score between 0 and 100.
  * It then calculates and displays the corresponding grade based on the score.
@@ -23,12 +23,23 @@
 
 /* A structure, ScoreInput, is defined to store the user input as a string and as a float, and to record whether the input is valid. */
 struct ScoreInput {
-    bool is_valid;
-    float score;
-    char str[10];
+    bool is_valid;  /**< Flag to indicate if the input is valid. */
+    float score;    /**< The user's score. */
+    char str[10];   /**< The user's input as a string. */
 };
 
-/* The is_float function checks if a given string can be converted to a float by ensuring it contains only digits and at most one decimal point. */
+/**
+ * Checks if a given string can be converted to a float.
+ *
+ * This function validates if the given string can be successfully converted to a float.
+ * It checks if the string contains only digits and at most one decimal point.
+ *
+ * Parameters:
+ *     str - The string to be validated.
+ *
+ * Returns:
+ *     true if the string is a valid float, false otherwise.
+ */
 bool is_float(const char* str) {
     bool point_seen = false;
     for (int64_t i = 0; i < strlen(str); i++) {
@@ -44,18 +55,31 @@ bool is_float(const char* str) {
     return true;
 }
 
-/* The get_valid_score function reads the user input, validates it, and returns a ScoreInput structure with the results. */
+/**
+ * Reads and validates the user's score input.
+ *
+ * This function prompts the user to enter their score and reads the input as a string.
+ * It then attempts to convert the string to a float and validates if it is a valid float using the is_float function.
+ * The function returns a ScoreInput structure containing the validated score and input string.
+ *
+ * Returns:
+ *     A ScoreInput structure containing the validated score and input string.
+ */
 struct ScoreInput get_valid_score() {
     struct ScoreInput user_score;
     user_score.is_valid = true;
 
-    /* First, the function attempts to read the user input as a string up to a newline character. If this fails, the function sets is_valid to false and returns. */
+    /* First, the function attempts to read the user input as a string up to a newline character.
+     * If this fails, the function sets is_valid to false and returns.
+     */
     if (scanf("%[^\n]", user_score.str) != 1) {
         user_score.is_valid = false;
         return user_score;
     }
 
-    /* Next, the function attempts to convert the string to a float. If this fails, the function sets is_valid to false. */
+    /* Next, the function attempts to convert the string to a float.
+     * If this fails, the function sets is_valid to false.
+     */
     user_score.is_valid = (sscanf(user_score.str, "%f", &user_score.score) == 1 ? true : false);
 
     /* If the conversion to float was successful, the function checks whether the string is a valid float using the is_float function. */
@@ -66,7 +90,17 @@ struct ScoreInput get_valid_score() {
     return user_score;
 }
 
-/* The main function of the program. */
+/**
+ * The main function of the program.
+ *
+ * This function prompts the user to enter their score between 0 and 100.
+ * It calls the get_valid_score function to validate the input score.
+ * If the input is not valid, an error message is printed, and the program terminates.
+ * If the input is valid, the program checks which range the score falls into and prints the score and the corresponding grade.
+ *
+ * Returns:
+ *     0 on successful execution.
+ */
 int main() {
     /* The user is prompted to enter their score. */
     printf("Enter your score: ");
